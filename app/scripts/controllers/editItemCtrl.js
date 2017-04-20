f0232d9649592bb289e3989411f1e3907922d3b2
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-20 14:05:05 -0700

    Feat: edit template

--- app/scripts/controllers/editItemCtrl.js
+++ app/scripts/controllers/editItemCtrl.js
@@ -2,7 +2,17 @@
     'use strict';
 
     angular.module('catalog')
-        .controller('editItemCtrl', [function() {
-            this.name = 'edit';
+        .controller('editItemCtrl', ['$stateParams', 'categories', function($stateParams, categories) {
+            categories.getCategories.call(this)
+                .then(function(response) {
+                    return response.data;
+                })
+                .then(function(data) {
+                    this.categories = data.categories;
+                    this.item = data.items.filter(function(item) {
+                        return item.itemName === $stateParams.item
+                            && item.categoryName === $stateParams.category;
+                    })[0];
+                }.bind(this));
         }]);
 })();
