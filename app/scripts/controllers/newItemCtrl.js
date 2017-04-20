295ac6d30aaf0cb06c28f8137d6bb5bb117b701f
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-20 13:49:02 -0700

    Feat: New Item Templating

--- app/scripts/controllers/newItemCtrl.js
+++ app/scripts/controllers/newItemCtrl.js
@@ -2,7 +2,14 @@
     'use strict';
 
     angular.module('catalog')
-        .controller('newItemCtrl', [function() {
-            this.name = 'new';
+        .controller('newItemCtrl', ['categories', function(categories) {
+            categories.getCategories.call(this)
+                .then(function(response) {
+                    return response.data;
+                })
+                .then(function(data) {
+                    this.categories = data.categories;
+                    console.log(this.categories);
+                }.bind(this));
         }]);
 })();
