cbf8d9e440525050cfda4d02a5636459766beaf7
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-25 09:14:49 -0700

    Feat: Hyperlinking on main page

--- app/scripts/controllers/categoryCtrl.js
+++ app/scripts/controllers/categoryCtrl.js
@@ -3,20 +3,14 @@
 
     angular.module('catalog')
         .controller('categoryCtrl', ['$stateParams', 'categories', function($stateParams, categories) {
-            /**
-             * Grabs category and item data for access in view
-             * Using `bind(this)` sets `this` key word as the controller object
-             */
-            categories.getCategories.call(this)
+             // Grabs category and item data for access in view
+             // Using `bind(this)` sets `this` key word as the controller object
+            categories.getCategoryItems.call(this, $stateParams.category)
                 .then(function(response) {
                     return response.data;
                 })
                 .then(function(data) {
-                    this.categories = data.categories;
-                    this.category = this.categories.filter(function(category) {
-                        return category.categoryName === $stateParams.category;
-                    })[0];
-                    this.items = this.category.items;
+                    this.items = data.Items;
                 }.bind(this));
         }]);
 })();
