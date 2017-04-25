0d5fd827cb72f0eae32fcc5441aeb6846d09ca8d
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-25 08:15:53 -0700

    fix: lowercase the itemName from db to ensure equality checking passes

--- app/scripts/controllers/editItemCtrl.js
+++ app/scripts/controllers/editItemCtrl.js
@@ -11,7 +11,7 @@
                 .then(function(data) {
                     this.items = data.Items;
                     this.item = this.items.filter(function(item) {
-                        return item.itemName === $stateParams.item;
+                        return item.itemName.toLowerCase() === $stateParams.item;
                     })[0];
                 }.bind(this));
 
