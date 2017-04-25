f944fd4dc50d37ad8df94eafe30a77b38d5ce886
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-25 09:23:49 -0700

    Feat: Read on specific item and hyperlinking

--- app/scripts/controllers/categoryCtrl.js
+++ app/scripts/controllers/categoryCtrl.js
@@ -3,9 +3,20 @@
 
     angular.module('catalog')
         .controller('categoryCtrl', ['$stateParams', 'categories', function($stateParams, categories) {
+            // Set $stateParams on model for access in view
+            this.stateParams = $stateParams;
+
              // Grabs category and item data for access in view
              // Using `bind(this)` sets `this` key word as the controller object
-            categories.getCategoryItems.call(this, $stateParams.category)
+            categories.getAll.call(this)
+                .then(function(response) {
+                    return response.data;
+                })
+                .then(function(data) {
+                    this.categories = data.Categories;
+                }.bind(this));
+
+            categories.getCategoryItems.call(this, this.stateParams.category)
                 .then(function(response) {
                     return response.data;
                 })
